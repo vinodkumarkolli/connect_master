1c7d47b9762180eaf1bf48070f132f5365fc9217
vinodkumarkolli <vinodkumarkolli@gmail.com>
2025-12-24 18:18:28 +0000

    chore: added Service Territory doctype

---
+++ connect_master/connect_master/doctype/service_territory/service_territory.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Vinod Kumar K and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+
+class ServiceTerritory(NestedSet):
+	pass
